646aa1adba9ad9018557e7eb062dcb1048de4184
francisohara24 <franciskohara@gmail.com>
2023-07-08 21:52:57 +0000

    feat: add 2_rfid_blink
    Write arduino sketch for selectively blinking LEDs based on RFID keycards passed to the MFRC522 reader

---
+++ 2_rfid_blink/2_rfid_blink.ino
@@ -0,0 +1,11 @@
+// This sketch turns on specific LEDs based on which RFID cards are detected by the MFRC522 RFID card reader.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
