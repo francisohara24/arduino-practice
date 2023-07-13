a285991d67cf959140cbc640be36dfd5c66641b4
francisohara24 <franciskohara@gmail.com>
2023-07-13 19:34:04 +0000

    Docs: update sketch description in opening comment

--- 2_rfid_blink/2_rfid_blink.ino
+++ 2_rfid_blink/2_rfid_blink.ino
@@ -1,7 +1,8 @@
-// This sketch turns on specific LEDs based on which RFID cards are detected by the MFRC522 RFID card reader.
+// This sketch turns on the onboard LED upon detecting an RFID card with a specific UID using the MFRC522 RFID card reader.
 
 void setup() {
   // put your setup code here, to run once:
+
 
 }
 
